d2d05ab8a83a33b0f64efa6dec141bdeee594ca3
imtiazmlbd <imtiaz.uddin@monstar-lab.com>
2022-04-12 14:28:46 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_unsplash_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_unsplash_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/.env.dart
@@ -0,0 +1,1 @@
+const String unsplashApiKey = "JgKYlCW0qWxlIdX2S6BqjSpmidfzJla6678Av3e0Yy0";
